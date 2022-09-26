34797445a4823aadac0b9def7727bc1eab4a1209
anup-thinkitive <anup.ghodake@thinkitive.com>
2022-09-26 19:53:55 +0530

    Created project for BlazorFullStackCrud

---
+++ BlazorFullStackCrud/BlazorFullStackCrud/Server/Data/DataContext.cs
@@ -0,0 +1,43 @@
+﻿namespace BlazorFullStackCrud.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+            modelBuilder.Entity<SuperHero>().HasData(
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Peter",
+                    Line1 = "abc",
+                    Line2 = "def",
+                    Locality = "ghi",
+                    Region ="jkl",
+                    Country = "mno",
+                    Longitude = 12,
+                    Latitude = 11,
+                    Contact = "123",
+                    //ContactId = ,
+                    CreatedAtUtc= DateTime.UtcNow,
+                    UpdatedAtUtc= DateTime.UtcNow,
+                    DeletedAtUtc= DateTime.UtcNow,
+                    Email = "pqrs",
+                    Salt = "tuvw",
+                    Password = "xyz"
+
+                }
+               
+            );
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+
+        public DbSet<Comic> Comics { get; set; }
+    }
+}
